50728efbae3bac628137ca32cf2bc249a49dc301
Klus3kk <lukasz.bielaszewski@onet.pl>
2025-12-18 18:46:38 +0100

    Finished creating Logger.

--- src/main/java/pl/put/poznan/transformer/logic/Logger.java
+++ src/main/java/pl/put/poznan/transformer/logic/Logger.java
@@ -26,7 +26,7 @@ public class Logger {
     /**
      * Creates a new Logger instance for the specified class.
      *
-     * @param clazz -> the class that will use the logger
+     * @param clazz the class that will use the logger
      */
     public Logger(Class<?> clazz) {
         this.logger = LoggerFactory.getLogger(clazz);
@@ -36,49 +36,112 @@ public class Logger {
     /**
      * Logs an informational message.
      *
-     * @param message -> the message to log
+     * @param message the message to log
      */
     public void info(String message) {
         logger.info(message);
     }
 
+    /**
+     * Logs informational message with parameters.
+     * @param message the message temple
+     * @param args the arguments to substitute into the message
+     */
     public void info(String message, Object... args) {
         logger.info(message, args);
     }
 
+    /**
+     * Logs a debug message.
+     * @param message message the message to log
+     */
     public void debug(String message) {
         logger.debug(message);
     }
 
+    /**
+     * Logs a debug message with parameters.
+     *
+     * @param message the message template
+     * @param args the arguments to substitute into the message
+     */
     public void debug(String message, Object... args) {
         logger.debug(message, args);
     }
 
+    /**
+     * Long a warning message.
+     *
+     * @param message the message to log
+     */
     public void warn(String message) {
         logger.warn(message);
     }
 
+    /**
+     * Logs a warning message with parameters.
+     *
+     * @param message the message template
+     * @param args the arguments to subsitute into the message
+     */
     public void warn(String message, Object... args) {
         logger.warn(message, args);
     }
 
+    /**
+     * Logs an error message.
+     *
+     * @param message the message to log
+     */
     public void error(String message) {
         logger.error(message);
     }
 
+    /**
+     * Logs an error message with parameters.
+     *
+     * @param message the message template
+     * @param args the argumnets to substitute into the message
+     */
     public void error(String message, Object... args) {
         logger.error(message, args);
     }
 
+    /**
+     * Logs an error message with parameters.
+     *
+     * @param message the message to log
+     * @param throwable the exception that occured
+     */
     public void error(String message, Throwable throwable) {
         logger.error(message, throwable);
     }
 
+    /**
+     * Logs entry into a method (debug level).
+     *
+     * @param methodName the name of the method being entered
+     */
     public void entering(String methodName) {
         logger.debug("Entering {}.{}", className, methodName);
     }
 
+    /**
+     * Logs exit from a method (debug level).
+     *
+     * @param methodName the name of the method being exited
+     */
     public void exiting(String methodName) {
         logger.debug("Exiting {}.{}", className, methodName);
+    }
+
+    /**
+     * Logs exit from a method with a return value (debug level).
+     *
+     * @param methodName the name of the method being exited
+     * @param result result the return value
+     */
+    public void exiting(String methodName, Object result) {
+        logger.debug("Exiting {}.{} with result: {}", className, methodName, result);
     }
 }
