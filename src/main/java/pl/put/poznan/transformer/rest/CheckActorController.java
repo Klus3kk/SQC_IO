ec312868f5f2ae6986a5cda2f0e3da222ab30df4
Krystian Czajkowski <czajkowski.krystian@gmail.com>
2025-12-19 11:40:41 +0100

    Add REST endpoint for actor validation in scenarios

---
+++ src/main/java/pl/put/poznan/transformer/rest/CheckActorController.java
@@ -0,0 +1,59 @@
+package pl.put.poznan.transformer.rest;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pl.put.poznan.transformer.logic.elements.JsonNodeToScenarioParser;
+import pl.put.poznan.transformer.logic.elements.Scenario;
+import pl.put.poznan.transformer.logic.visitor.CheckActorVisitor;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/scenario")
+public class CheckActorController {
+    /* EXAMPLE REQUEST
+    {
+        "type": "scenario",
+        "title": "Dodanie książki",
+        "system actor": "System",
+        "actors": ["Bibliotekarz"],
+        "elements": [{
+            "type": "step",
+            "content": "Bibliotekarz wybiera opcje dodania nowej pozycji książkowej"
+            },
+            {
+            "type": "step",
+            "content": "Wybiera opcje dodania nowej pozycji książkowej"
+            },
+            {
+            "type": "step",
+            "content": "IF Bibliotekarz potwierdza dane"
+            }
+        ]
+    }
+     */
+    @PostMapping("/checkActors")
+    public ResponseEntity<Map<String, Object>> checkActors(@RequestBody JsonNode json) {
+        Scenario scenario = JsonNodeToScenarioParser.parseScenario(json);
+
+        if (scenario == null) {
+            Map<String, Object> error = new HashMap<>();
+            error.put("error", "Failed to parse json. Send valid json next time.");
+            return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+        }
+
+        CheckActorVisitor visitor = new CheckActorVisitor();
+        scenario.accept(visitor);
+
+        List<String> invalidSteps = visitor.getInvalidSteps();
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("invalid steps", invalidSteps);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
