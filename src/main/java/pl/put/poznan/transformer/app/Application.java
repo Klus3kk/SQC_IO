9030746e57b711964e9354b7841378918f4d5877
Aviuss <konradbb09@gmail.com>
2025-12-21 16:18:44 +0100

    delete old comments

--- src/main/java/pl/put/poznan/transformer/app/Application.java
+++ src/main/java/pl/put/poznan/transformer/app/Application.java
@@ -17,23 +17,6 @@ public class Application {
     }
 
     public static void main(String[] args) {
-        /* EXAMPLE CODE
-        CountStepsVisitor visitator = new CountStepsVisitor();
-        Scenario scenariusz = new Scenario("testowy");
-        Step k1 = new Step();
-        Step k2 = new Step();
-        Step k3 = new Step();
-        Step k4 = new Step();
-
-        scenariusz.add(k1);
-        scenariusz.add(k2);
-        scenariusz.add(k3);
-        scenariusz.add(k4);
-
-        scenariusz.accept(visitator);
-
-        System.out.println(visitator.returnStepCount());
-        */
         SpringApplication.run(Application.class, args);
     }
 
